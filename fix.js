59ca4b7dec0b673e9fb84e9f8d24e5c32a2460c7
Richard Meng <richard.meng@calix.com>
2018-07-03 02:47:49 -0500

    rxjs rocks

--- fix.js
+++ fix.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const DEBUG = true;
 
 const program = require('commander');
 const req = require('sync-request');
@@ -7,12 +6,13 @@ const Mongo = require('mongodb').MongoClient;
 const assert = require('assert');
 const Rx = require('rxjs/Rx');
 const Observable = require('rxjs/Observable').Observable;
+const flatMap = require('rxjs/operators').flatMap;
 const _ = require('underscore');
 
 program.version('0.10.0', '-v, --version')
   .option('-m, --mongo-url <url>', 'MongoDB connection URL')
   .option('-d, --db-name [value]', 'Database name', 'sxa')
-  .option('-f, --fix', 'Delete subscribers in billing')
+  .option('-f, --fix', 'Fix subscribers in billing')
   .option('-o, --org-id <id>', 'Organization ID')
   .option('--debug', 'Print debug logs')
   .parse(process.argv);
@@ -33,115 +33,67 @@ console.log('Fix subscriber - ' + program.fix);
 console.log('');
 
 // process.exit(1);
-const doFix = function (sub, subBillingCol) {
-  let billingId = sub.subBilling._id;
-  let cscId = sub.subCsc._id;
-  let q = { _id: billingId };
-  let u = { '$set': { subscriberId: cscId } };
-
-  if (program.debug) console.log('Update with query - q: ' + JSON.stringify(q) + ', u: ' + JSON.stringify(u));
-  if (!program.fix) return Observable.of({ok: 0});
-
-  let updateRx = Observable.bindNodeCallback((q, u, cb) => subBillingCol.findOneAndUpdate(q, u, cb));
-  return updateRx(q, u);
-}
-
-const findCscSubscriber = function (subBilling, subCol) {
-  return Observable.create(observer => {
-    let customIdArray = subBilling.csckey.split('||');
-    let query = {
-      'orgId': program.orgId,
-      '$or': _.map(customIdArray, cid => { return {'customId': cid} })
-    };
-    if (program.debug) console.log('Find CSC subscriber with customId, q: ' + JSON.stringify(query));
-
-    let findRx = Observable.bindNodeCallback((q, cb) => subCol.find(q).toArray(cb));
-    findRx(query).subscribe(subs => {
-      if (subs.length == 0) {
-        if (program.debug) console.log('Billing Subscriber has no CSC subscriber, ' + subBilling._id)
-      } else {
-        if (program.debug) console.log('Billing Subscriber has CSC subscriber, ' + subBilling._id)
-        observer.next({
-          subBilling: subBilling,
-          subCsc: subs[0]
-        });
-      }
-      observer.complete();
-    });
-  });
-}
-
 
 const MongoConnectRx = Observable.bindNodeCallback(Mongo.connect);
-
 MongoConnectRx(program.mongoUrl).subscribe(client => {
   const db = client.db(program.dbName);
+  assert.ok(db);
+
   const subBillingCol = db.collection('sxa-subscriber-billing');
   const subCol = db.collection('sxa-subscribers');
   assert.ok(subBillingCol);
   assert.ok(subCol);
 
   let quit = function (c) { process.exit(c); client.close(); }
+  let debug = (s) => program.debug ? console.debug('DEBUG > ' + s) : '';
 
   let query = {
     'orgid': Number(program.orgId),
     'subscriberId': {'$exists': false}
   }
 
-  if (program.debug) console.info('Query sxa-subsciber-billing: ' + JSON.stringify(query, null, 2));
+  debug('Query sxa-subsciber-billing: ' + JSON.stringify(query, null, 2));
 
   let findRx = Observable.bindNodeCallback((q, cb) => subBillingCol.find(q).toArray(cb));
   findRx(query).subscribe(subscribers => {
-    Observable.create(observer => {
-      observer.count = subscribers.length;
-      Observable.from(subscribers).subscribe({
-        next(s) {
-          if (program.debug) console.log('Subscriber, ' + s._id + ', csckey: ' + s.csckey);
-          let subBilling = {
-            _id: s._id,
-            name: s.name,
-            csckey: s.csckey
-          };
+    Observable.from(subscribers)
+      .map(s => ({ _id: s._id, name: s.name, csckey: s.csckey }))
+      .flatMap(s => {
+        let customIdArray = s.csckey.split('||');
+        let query = {
+          'orgId': program.orgId,
+          '$or': _.map(customIdArray, cid => ({'customId': cid}))
+        };
 
-          let fixObserver = {
-            sub: null,
-            next(s) { this.sub = s; },
-            complete() {
-              if (this.sub != null) {
-                observer.next(this.sub)
-              }
-              observer.count--;
-              if (program.debug) console.log('Subscribers left: ' + observer.count);
-              if (observer.count == 0) {
-                observer.complete();
-              }
-            }
-          };
-          findCscSubscriber(subBilling, subCol).subscribe(fixObserver);
-        },
-        complete() {
-          if (program.debug) {
-            console.log('Scanned ' + subscribers.length + ' subscribers');
-            console.log('----------------------------------------------');
-          }
+        debug('Subscriber Billing, ' + s._id + ', csckey: ' + s.csckey);
+        debug('Query CSC Subscriber, q: ' + JSON.stringify(query));
+
+        let findRx = Observable.bindNodeCallback((q, cb) => subCol.find(q).toArray(cb));
+        return Observable.zip(Observable.of(s), findRx(query), (b, c) => ({ billing: b, csc: c }));
+      })
+      .filter(s => s.csc.length > 0)
+      .map(s => ({ billing: s.billing, csc: _.map(s.csc, s0 => ({ _id: s0._id, customId: s0.customId, name: s0.name }))[0] }))
+      .flatMap(sub => {
+        let q = { _id: sub.billing._id };
+        let u = { '$set': { subscriberId: sub.csc._id } };
+
+        debug('Update Billing - q: ' + JSON.stringify(q) + ', u: ' + JSON.stringify(u));
+
+        let updateObservable;
+        if (!program.fix) updateObservable = Observable.of({ok: 0});
+        else {
+          let updateRx = Observable.bindNodeCallback((q, u, cb) => subBillingCol.findOneAndUpdate(q, u, cb));
+          updateObservable = updateRx(q, u);
         }
+        return Observable.zip(Observable.of(sub), updateObservable, (s, r) => ({sub: s, res: r}));
+      })
+      .concat(Observable.of({end: true}))
+      .subscribe(r => {
+        debug('Result - ' + JSON.stringify(r, null, 2));
+
+        if (r.end) quit(0);
+        console.log('Fixed (update) ' + r.sub.billing._id + ', subscriberId: ' + r.sub.csc._id + ', result -> ' + r.res.ok);
       });
-    }).subscribe({
-      sum: 0,
-      next(sub) {
-        let _self = this;
-        _self.sum++;
-        let fixer = doFix(sub, subBillingCol)
-        if (fixer != null) fixer.subscribe(res => {
-          console.log('Fixed (update) ' + sub.subBilling._id + ', subId: ' + sub.subCsc._id + ', result -> ' + res.ok);
-        });
-      },
-      complete() {
-        console.log('--------------------------------------------------');
-        console.log(this.sum + ' subscribers processed');
-        //quit(0);
-      }
-    });
   },
   err => {
     console.error('Failed to query, ' + err);
